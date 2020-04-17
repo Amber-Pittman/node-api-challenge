75f9780243fb9d99c9bc316445076afc053dfc21
Amber-Pittman <amber@amberpittman.com>
2020-04-17 19:35:23 -0400

    Imported logger. Added middleware functions

--- index.js
+++ index.js
@@ -15,12 +15,27 @@ Go code!
 
 const express = require("express")
 const cors = require("cors")
+const logger = require("./middleware/logger")
 
 const server = express()
 const port = 4000
 
 server.use(express.json())
 server.use(cors)
+server.use(logger({ format: "long"}))
+
+server.use((req, res) => {
+    res.status(404).json({
+        message: "Route was not found.",
+    })
+})
+
+server.use((error, req, res, next) => {
+    console.log(error)
+    res.status(500).json({
+        message: "Something went wrong. Cannot provide more information.",
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server running at http:localhost:${port}`)
